ab3d6c7c81b86cffa7c3fc02b982cad69a2a2211
daniel <daniel17429@gmail.com>
2024-10-11 00:39:10 +0500

    Added player

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -14,12 +18,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     BLACK = (0, 0, 0)
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
